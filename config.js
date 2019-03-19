3a299bbc50e4ce4402facc0ee957cd5a2337a2dd
Ryan Chu <tianyuan.chu@gmail.com>
2019-03-19 00:33:14 -0700

    updates

---
+++ config.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports = {
+    url: "https://lumen.netlify.com",
+    title: "React Blog for Session 652",
+    subtitle: "",
+    copyright: "© All rights reserved.",
+    disqusShortname: "",
+    postsPerPage: 4,
+    googleAnalyticsId: "",
+    menu: [
+        {
+            label: "Articles",
+            path: "/"
+        },
+        {
+            label: "About React Nanodegree",
+            path: "/pages/about"
+        }
+    ],
+    author: {
+        name: "Ryan Chu",
+        photo: "/react.png",
+        bio: "Use React in everyday life",
+        contacts: {
+            github: "TianyuanC"
+        }
+    }
+};
---
+++ content/posts/2019-03-19---Setting-up-MyReads.md
@@ -0,0 +1,12 @@
+---
+title: Setting up MyReads
+date: "2019-03-19T23:46:37.121Z"
+template: "post"
+draft: false
+slug: "/posts/setting-up-myreads/"
+category: "Projects"
+tags:
+    - "MyReads"
+    - "Project"
+description: "Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum."
+---
